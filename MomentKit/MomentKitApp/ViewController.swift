04d0ff6766932451a90fd342b77fbdb672b42837
lee-ukjeong <lee.ukjeong@navercorp.com>
2020-08-21 16:18:37 +0900

    .

---
+++ MomentKit/MomentKitApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MomentKitApp
+//
+//  Created by Ukjeong Lee on 2020/08/21.
+//  Copyright © 2020 Ukjeong Lee. All rights reserved.
+//
+
+import UIKit
+import MomentCommon
+import MomentEditor
+import MomentPlayer
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
